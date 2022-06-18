2d9ded9ab6ef9f5ccd363995c98eee79a7137a45
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-06-18 21:57:35 +0400

    feat: add constants and start command

---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const HTTP_STATUS_CODES = {
+	OK: 200,
+	INTERNAL_SERVER_ERROR: 500,
+	NOT_FOUND: 404,
+};
+
+export const DEFAULT_HEADERS = {
+	'Content-Type': 'application/json'
+};
+
+export const HTTP_RESPONSE_MESSAGES = {
+	INTERNAL_SERVER_ERROR: '500 Internal Server Error'
+};
